deeb38d79d598fc1d4cec253bd84df6335103f57
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-10-18 11:13:46 -0400

    added helper files

---
+++ MaxHomophones.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Dictionary;
+
+public class MaxHomophones {
+public static void main(String[] args) {
+    
+}
+
+public static void displayMaxHomophones(int nLines) {
+
+}
+
+private static void readAndReturnDict(File file) {
+
+}
+    
+}
